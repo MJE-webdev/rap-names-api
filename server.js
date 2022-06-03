9ab74444cf75bc59906232cd2579ea947d5f2b57
Melissa Estrella <melissa.estrella@gmail.com>
2022-06-02 22:46:17 -0400

    update to backend.

--- server.js
+++ server.js
@@ -3,28 +3,29 @@ const app = express()
 const cors = require('cors')
 const PORT = 8000
 
+app.use(cors())
 
 const rappers = {
-'21 savage': {
-    'age':  29,
-    'birthName': 'Sheyaa Bin Abraham-Joseph',
-    'birthLocation': 'London, England'
-},
-'chance the rapper': {
-    'age': 29,
-    'birthName': 'Chancelor Bennett',
-    'birthLocation': 'Chicago, Illinois'
-},
-'unknown': {
-    'age': 0,
-    'birthName': 'unknown',
-    'birthLocation': 'unknown'
-}
+    '21 savage': {
+        'age':  29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
 }
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
-app.get('/api/:name', (request, response) => {
+app.get('/api/:name', (request, response) =>{
     const rapperName = request.params.name.toLowerCase()
     console.log(rappers[rapperName])
     if (rappers [rapperName]) {
@@ -32,7 +33,7 @@ app.get('/api/:name', (request, response) => {
     }else{
         response.json(rappers['unknown'])
     }
-    response.json(rappers)
+
 })
 app.listen(process.env.PORT || PORT, () => {
     console.log (`The server is now running on port ${PORT}!  Betta go catch it!`)
